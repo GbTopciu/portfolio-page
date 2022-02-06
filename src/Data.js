f4581ad8ade936b2f0aa6692a0f57bcaacc13ce0
Gabriel Topciu <gb.topciu@gmail.com>
2022-02-06 21:39:49 +0000

    Created the data.js file which will hold an array of objects required for the Cards. Also installed uuidv4 which will allow for a dynamic Id.

---
+++ src/Data.js
@@ -0,0 +1,15 @@
+import { v4 as uuidv4 } from "uuid";
+
+const Data = () => {
+    return [
+      {
+        name: "",
+        description: "Misha, NDO, Flozigg",
+        cover:
+          "https://chillhop.com/wp-content/uploads/2022/01/6881c7456483ab8ea364152a594942a20669a058-1024x1024.jpg",
+        id: uuidv4(),
+        active: false,
+      },
+    ]}
+
+    export default Data;
