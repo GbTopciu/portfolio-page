dde23f9864f626404f26405cba59181cdb1831aa
Gabriel Topciu <gb.topciu@gmail.com>
2022-02-09 23:34:54 +0000

    updated the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,14 +3,14 @@ import React from "react";
 const About = () => {
   return (
     <div className="h-screen">
-      <div className="flex flex-col justify-center items-center h-1/2	">
+      <div className="flex flex-col justify-center items-center h-1/2">
         <h2 className="text-pink-500 text-5xl mb-5">THE JOURNEY</h2>
-        <p className="text-lime-500 text-xl">
+        <p className="text-lime-500 text-xl text-center ">
           I Know React. In Fact I Wrote A Bunch Of Websites Using It
         </p>
       </div>
-      <div className="w-screen flex justify-center items-center h-1/2 bg-gray-300">
-        <section className="text-xl text-center w-1/2 ">
+      <div className="flex justify-center items-center  h-1/2 bg-gray-300">
+        <section className="text-xl text-center bg-gray-300">
           It all began when I was required to create my first Excel report on a
           daily basis. This is getting boring, I thought... There must be a
           better way. Then I taught myself to code with VBA. Not only did I end
