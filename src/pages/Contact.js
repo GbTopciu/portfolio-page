ed53a6ef3a47b06da1f0d0e1c445732b1b01dfb5
Gabriel Topciu <gb.topciu@gmail.com>
2022-02-09 22:48:00 +0000

    need to update contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
   return <div>
-    <h2>This is the contact page</h2>
+    <h2 className='flex justify-center items-center h-screen text-6xl text-white'>This is the contact page. Contact form to follow (WIP)</h2>
   </div>;
 };
 
